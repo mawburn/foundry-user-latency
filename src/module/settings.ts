25fc36ba224308066e7ec973da75c4758b4184cc
mawburn <mawburn7@gmail.com>
2021-12-30 12:04:52 -0500

    udpate to 0.9.0

--- src/module/settings.ts
+++ src/module/settings.ts
@@ -2,8 +2,8 @@ import { MODULE_NAME } from '../constants'
 
 export const registerSettings = () => {
   ;(game as Game).settings.register(MODULE_NAME, 'pingInterval', {
-    name: `${game.i18n.localize("PINGLOGGER.Interval")}`,
-    hint: `${game.i18n.localize("PINGLOGGER.IntervalHint")}`,
+    name: `${(game as any).i18n.localize('PINGLOGGER.Interval')}`,
+    hint: `${(game as any).i18n.localize('PINGLOGGER.IntervalHint')}`,
     type: Number,
     // @ts-ignore
     range: {
